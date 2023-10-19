dc5540a13931725e7c0b5fa6aa5e6ffc63fbdb19
anisamuema <anisa.muema.school@gmail.com>
2023-10-19 14:04:34 +0300

    Home details finished

---
+++ index.js
@@ -0,0 +1,13 @@
+// active menu
+let menuIcon = document.querySelector(".menu-icon");
+
+menuIcon.addEventListener("click",()=>{
+    menuIcon.classList.toggle("active")
+})
+
+// rotate text js code
+let text = document.querySelector(".text p");
+
+text.innerHTML = text.innerHTML.split("").map((char,i)=>
+    `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
+).join("")
